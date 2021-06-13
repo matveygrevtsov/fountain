a7c25d6e946b179ee8b20efcea6d9d8d34e6ed76
matveygrevtsov <matveigrevtsov@yandex.ru>
2021-06-13 17:45:28 +0300

    Some bugs fixed

--- docs/dat_gui/dat.gui.init.js
+++ docs/dat_gui/dat.gui.init.js
@@ -1,26 +1,26 @@
 function FontainParams() {
-    this.animationSpeed = 5;
+    this.animationSpeed = 15;
     this.power = 100;
     this.itemsNumber = 15;
     this.spreadAngle = 180;
     this.gravity = 10;
     this.minItemsSize = 100;
     this.maxItemsSize = 380;
-    this.rotationSpeed = 1;
+    this.rotationSpeed = 7;
 };
 
 window.onload = function() {
     var params = new FontainParams();
     var gui = new dat.GUI();
 
-    var controllerAnimationSpeed = gui.add(params, 'animationSpeed', 0, 25);
+    var controllerAnimationSpeed = gui.add(params, 'animationSpeed', 0, 50);
     var controllerPower = gui.add(params, 'power', 0, 300);
     var controllerItemsNumber = gui.add(params, 'itemsNumber', 0, 100);
     var controllerSpreadAngle = gui.add(params, 'spreadAngle', 0, 180);
     var controllerGravity = gui.add(params, 'gravity', 0, 20);
     var controllerMinItemsSize = gui.add(params, 'minItemsSize', 0, 1000);
     var controllerMaxItemsSize = gui.add(params, 'maxItemsSize', 0, 1000);
-    var controllerRotationSpeed = gui.add(params, 'rotationSpeed', 0, 15);
+    var controllerRotationSpeed = gui.add(params, 'rotationSpeed', 0, 20);
 
     controllerAnimationSpeed.onChange((value) => {
         animationSpeed = value
--- docs/fountain.js
+++ docs/fountain.js
@@ -1,14 +1,13 @@
 const canvas = document.querySelector("#fountain");
 const imgSkins = document.querySelectorAll("#fountainImgs img");
 
-let animationSpeed = 5;
+let animationSpeed = 15;
 let power = 100;
 let itemsNumber = 15;
 let spreadAngle = 180;
 let itemsSize = [100, 380];
 let g = 10;
-let rotationSpeed = 1;
-let period = 0;
+let rotationSpeed = 7;
 
 const context = canvas.getContext("2d");
 let x_0;
@@ -29,7 +28,7 @@ function setCanvasSize() {
 window.addEventListener("resize", setCanvasSize);
 
 class CanvasItem {
-    constructor(speed_x, speed_y, alpha, skin, width, id) {
+    constructor(speed_x, speed_y, alpha, skin, width) {
         this.t = 0;
         this.x = canvas.width - width / 2;
         this.y = canvas.height;
@@ -38,7 +37,10 @@ class CanvasItem {
         this.alpha = alpha;
         this.skin = skin;
         this.width = width;
-        this.id = id;
+    }
+
+    incrementTime() {
+        this.t += animationSpeed / 100;
     }
 
     move() {
@@ -46,7 +48,7 @@ class CanvasItem {
         this.y =
             y_0 - this.v_0y * this.t * Math.sin(this.alpha) +
             (g * this.t * this.t) / 2;
-        this.t += animationSpeed * period / 1000;
+        this.incrementTime();
         if ((this.y > y_0 + 2 * this.width) || (this.y < -2 * this.width) || (this.x > canvas.width + 2 * this.width) || (this.x < -2 * this.width)) {
             this.resetParams();
         }
@@ -59,22 +61,12 @@ class CanvasItem {
         this.alpha = randomFromInterval(dispersion());
         this.width = randomFromInterval(itemsSize);
     }
-
-    render(timestamp) {
-        context.save();
-        context.translate(this.x + this.width / 2, this.y + this.width / 2);
-        context.rotate(timestamp / 1000 * rotationSpeed * (2 * (this.id % 2) - 1));
-        context.translate(-this.x - this.width / 2, -this.y - this.width / 2);
-        context.drawImage(imgSkins[this.skin], this.x, this.y, this.width, this.width);
-        context.restore();
-        this.move();
-    }
 }
 
 const initCanvasItems = () => {
     canvasItems = [];
     for (let i = 0; i < itemsNumber; i++) {
-        canvasItems.push(new CanvasItem(power, power, randomFromInterval(dispersion()), i % imgSkins.length, randomFromInterval(itemsSize), i));
+        canvasItems.push(new CanvasItem(power, power, randomFromInterval(dispersion()), i % imgSkins.length, randomFromInterval(itemsSize)));
     }
 }
 
@@ -82,17 +74,17 @@ initCanvasItems();
 
 requestAnimationFrame(tick);
 
-let frameIndex = 0;
-let prevTimestamp = 0;
-
 function tick(timestamp) {
     requestAnimationFrame(tick);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    if (frameIndex > 2) {
-        canvasItems.forEach(item => item.render(timestamp));
-    } else {
-        period = timestamp - prevTimestamp;
-        prevTimestamp = timestamp;
-        frameIndex++;
-    }
+    canvasItems.forEach((item, index) => {
+        context.beginPath();
+        context.save();
+        context.translate(item.x + item.width / 2, item.y + item.width / 2);
+        context.rotate(timestamp / 1000 * rotationSpeed * (2 * (index % 2) - 1));
+        context.translate(-item.x - item.width / 2, -item.y - item.width / 2);
+        context.drawImage(imgSkins[item.skin], item.x, item.y, item.width, item.width);
+        context.restore();
+        item.move();
+    });
 }
